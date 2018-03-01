c04a93674b0ca9366d711fc27556fdb3d9b15e8e
lena-1 <lena@programmer.net>
2018-03-01 17:50:09 -0500

    _

--- express_S1_L7-14/app.js
+++ express_S1_L7-14/app.js
@@ -3,11 +3,20 @@ var app = express();
 var path =require('path');
 var path =require('path');
 
+//Define port for express to run on
 app.set('port', 3000);
 
 
+// Add middleware to console log every request
+app.use(function(req,res,next){
+	console.log(req.method, req.url);
+	next();
+});
+
+//Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Add some routing
 app.get('/json', function(req, res){
   console.log("GET the json");
   res
@@ -15,17 +24,16 @@ app.get('/json', function(req, res){
     .json({"jsonData": true});
 });
 
-
-var server= app.listen(app.get('port'), function(){
-  var port = server.address().port;
-  console.log('Magic happens on port '+ port);
-});
-
-
 app.get('/file', function(req, res){
   console.log("GET the file");
   res
     .status(200)
-    // .sendFile(path.join(__dirname, 'app.js'));
-    .sendFile(path.join(__dirname, 'car.png'));
+    .sendFile(path.join(__dirname, 'car.png'));  //or app.js instead of car.png
+});
+
+
+//Make express server listen for requests
+var server= app.listen(app.get('port'), function(){
+  var port = server.address().port;
+  console.log('Magic happens on port '+ port);
 });
