87e17801f1e88f81878d1f4d773285b1110b1eb3
lena-1 <lena@programmer.net>
2018-03-01 22:32:23 -0500

    finished l13-dynamic routes-URLparams

--- express_S1_L7-14/api/controllers/hotels.controllers.js
+++ express_S1_L7-14/api/controllers/hotels.controllers.js
@@ -4,9 +4,19 @@ var hotelData = require('../data/hotel-data.json');
 
 //Defined 1st controller
 module.exports.hotelsGetAll = function(req, res) {
-  console.log('GET the hotels');
+  console.log('-->GET the hotels');
   res
     .status(200)
     //.json({ "jsondata" : "GET received" });
     .json(hotelData);
+};
+
+//Defined 2nd controller
+module.exports.hotelsGetOne = function(req, res) {
+  var hotelId= req.params.hotelId;
+  var thisHotel= hotelData[hotelId];
+  console.log('-->GET hotelId= ', hotelId);
+  res
+    .status(200)
+    .json(thisHotel);
 };
