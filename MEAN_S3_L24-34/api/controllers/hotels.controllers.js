698f51210e6b57bb3d402334644fe3a91281bca1
lena-1 <lena@programmer.net>
2018-03-06 10:45:55 -0500

    finished l30-nearest hotel-working with geospatial data

--- MEAN_S3_L24-34/api/controllers/hotels.controllers.js
+++ MEAN_S3_L24-34/api/controllers/hotels.controllers.js
@@ -6,6 +6,77 @@
 // var hotelData = require('../data/hotel-data.json');
 
 //get data from a mongoDB via mongoose driver
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
+
+
+
+
+var runGeoQuery = function(req, res) {
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+
+
+  // A geoJSON point
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+
+
+  var geoOptions = {
+    spherical : true,
+    maxDistance : 2000,
+    num : 5
+  };
+
+
+
+  //TODO: research geoNear(point, geoOptions, function(err, results, stats)
+  Hotel
+    .aggregate([{'$geoNear': {
+            			'near': point,
+            			'spherical': true,
+            			'distanceField': 'dist',
+            			'maxDistance': 200000,
+            			'num' : 5
+            			 }
+            		}],
+        function(err, results) {  //, stats
+          if (err){
+              console.log("error finding hotels");
+              res
+                  .status(500)
+                  .json(err);
+          }else if (results.length === 0){
+              res
+                  .status(404)
+                  .json({
+                      "message": "No Hotels Found in this Location"
+                  });
+          }else{
+          console.log('Geo results',results);
+          // console.log('Num results', results.length);
+          // console.log('Geo stats', stats); //stats is undefined
+          res
+              .status(200)
+              .json(results);
+          }
+          });
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -17,18 +88,18 @@
 module.exports.hotelsGetAll = function(req, res) {
   console.log('--GET the hotels');
   console.log(req.query);
-  
-  
-  //Working with MongoDB data via Mongoose driver
-  var mongoose = require('mongoose');
-  var Hotel = mongoose.model('Hotel');
-  
-  
-  
+
+
+  // //Working with MongoDB data via Mongoose driver
+  // var mongoose = require('mongoose');
+  // var Hotel = mongoose.model('Hotel');
+
+
+
   // //Working with MongoDB data via native driver
   // var db = dbconn.get();  //console.log("db", db);
   // var collection = db.collection('hotels');
-  
+
   // //returns ALL mongo docs
   // collection
   //   .find()
@@ -38,33 +109,39 @@ module.exports.hotelsGetAll = function(req, res) {
   //       .status(200)
   //       .json(docs);
   // });
-  
-  
+
+
   //returns a subset of mongo docs
   var offset = 0;
   var count = 5;
-  
+
+
+  if (req.query && req.query.lat && req.query.lng) {
+  runGeoQuery(req, res);
+  return;
+  }
+
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
   }
   if (req.query && req.query.count) {
     count = parseInt(req.query.count, 10);
   }
-  
-  
+
+
   //working with collection via Mongoose driver
   Hotel
   .find()
   .skip(offset)
-  .limit(count) 
+  .limit(count)
   .exec(function(err, hotels) {
     console.log("Found hotels", hotels.length);
     res
       .json(hotels);
   });
-  
-  
-  
+
+
+
   //working with collection via native driver
   // collection
   //   .find()
@@ -76,7 +153,7 @@ module.exports.hotelsGetAll = function(req, res) {
   //       .status(200)
   //       .json(docs);
   // }); //default=Found 5 hotels
-  
+
 
   // //Working with JSON in the app. Return a subset of docs
   // var returnData;
@@ -112,14 +189,14 @@ module.exports.hotelsGetOne = function(req, res) {
   // var db = dbconn.get();
   // var collection = db.collection('hotels');
   var id = req.params.hotelId;
-  
-  
-  
-  //via Mongoose driver:
-  var mongoose = require('mongoose');
-  var Hotel = mongoose.model('Hotel');
-  
-  
+
+
+
+  // //via Mongoose driver:
+  // var mongoose = require('mongoose');
+  // var Hotel = mongoose.model('Hotel');
+
+
   console.log('--GET hotel id', id);
 
   //via native driver:
@@ -131,7 +208,7 @@ module.exports.hotelsGetOne = function(req, res) {
   //       .status(200)
   //       .json(doc);
   // });
-  
+
   //via mongoose driver:
   Hotel
   .findById(id)
@@ -140,15 +217,15 @@ module.exports.hotelsGetOne = function(req, res) {
       .status(200)
       .json(doc);
   });
-  
-  
-  
+
+
+
 };
 
 
 
 
-//3rd controller: 
+//3rd controller:
 // //--data grabbed from online form and shown in console
 // module.exports.hotelsAddOne = function(req, res) {
 //   console.log("--POST new hotel");
@@ -174,7 +251,7 @@ module.exports.hotelsAddOne = function(req, res) {
       //console.log("Hotel added", response);
       console.log("Hotel added", response.ops);
       res
-        .status(201) 
+        .status(201)
         .json(response.ops);
     });
   } else {
@@ -184,4 +261,3 @@ module.exports.hotelsAddOne = function(req, res) {
       .json({ message : "Required data missing from body" });
   }
 };
-
