93feb5be2d015a9e63c2cd8ca11cb75aa4b45790
lena-1 <lena@programmer.net>
2018-03-06 16:20:13 -0500

    finished l31- error trapping

--- MEAN_S3_L24-34/api/controllers/hotels.controllers.js
+++ MEAN_S3_L24-34/api/controllers/hotels.controllers.js
@@ -16,6 +16,15 @@ var runGeoQuery = function(req, res) {
   var lng = parseFloat(req.query.lng);
   var lat = parseFloat(req.query.lat);
 
+  //if in the query string, latitu or longit isNotANumber
+  if (isNaN(lng) || isNaN(lat)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied in querystring, lng, lat must both be numbers"
+      });
+    return;
+  }
 
   // A geoJSON point
   var point = {
@@ -114,7 +123,7 @@ module.exports.hotelsGetAll = function(req, res) {
   //returns a subset of mongo docs
   var offset = 0;
   var count = 5;
-
+  var maxCount = 50;
 
   if (req.query && req.query.lat && req.query.lng) {
   runGeoQuery(req, res);
@@ -128,17 +137,45 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
+  //if in the query string, offset or count isNotANumber
+  if (isNaN(offset) || isNaN(count)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied in querystring, count and offset must both be numbers"
+      });
+    return;
+  }
+
+  //more error trapping
+  if (count > maxCount) {
+    res
+      .status(400)
+      .json({
+        "message" : "Count limit of " + maxCount + " exceeded"
+      });
+    return;
+  }
 
   //working with collection via Mongoose driver
   Hotel
-  .find()
-  .skip(offset)
-  .limit(count)
-  .exec(function(err, hotels) {
-    console.log("Found hotels", hotels.length);
-    res
-      .json(hotels);
-  });
+    .find()
+    .skip(offset)
+    .limit(count)
+    .exec(function(err, hotels) {
+      console.log(err);
+      console.log(hotels);
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        console.log("Found hotels", hotels.length);
+        res
+          .json(hotels);
+      }
+    });
 
 
 
@@ -210,13 +247,37 @@ module.exports.hotelsGetOne = function(req, res) {
   // });
 
   //via mongoose driver:
+  // Hotel
+  // .findById(id)
+  // .exec(function(err, doc) {
+  //   res
+  //     .status(200)
+  //     .json(doc);
+  // });
+
+  //error checking
   Hotel
-  .findById(id)
-  .exec(function(err, doc) {
-    res
-      .status(200)
-      .json(doc);
-  });
+    .findById(id)
+    .exec(function(err, doc) {
+      var response = {
+        status : 200,
+        message : doc
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        console.log("HotelId not found in database", id);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + id
+        };
+      }
+      res
+        .status(response.status)
+        .json(response.message);
+    });
 
 
 
